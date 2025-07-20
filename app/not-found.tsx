9c5bbb93cf0f8ecd3a497832ac9cd5d8d0ea8801
DESKTOP-HA97GMI\SSAFY <gnaao1005@gmail.com>
2025-07-20 20:52:54 +0900

    until 2.2 Not Found Routes

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Page N\ot Found!</h1>
+    </div>
+  );
+}
