a289e1b960e88c36a69c5dfec34b06bc6bee70c0
DESKTOP-HA97GMI\SSAFY <gnaao1005@gmail.com>
2025-07-22 08:57:50 +0900

    Metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,15 @@
+import { Metadata } from "next";
 import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not found",
+};
 
 export default function NotFound() {
   return (
     <div>
       <Navigation />
-      <h1>Page N\ot Found!</h1>
+      <h1>Page Not Found!</h1>
     </div>
   );
 }
