c12e529b14aaea660b158b7be5878af24c4b3936
구하린 <gnaao1005@gmail.com>
2025-07-23 21:51:58 +0900

    Error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,6 +3,8 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
+  // await new Promise(resolve => setTimeout(resolve, 1000));
+  // throw new Error("something went wrong");
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
