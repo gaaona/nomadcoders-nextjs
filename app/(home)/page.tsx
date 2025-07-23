b41a61ce04ad83c7b52a991c1490339eb8771e44
DESKTOP-HA97GMI\SSAFY <gnaao1005@gmail.com>
2025-07-23 11:02:34 +0900

    Loading Components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,8 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  const response = await fetch(URL);
+  await new Promise((resolve) => setTimeout(resolve, 5000)); // 5초 동안 멈춤(백엔드 단에서)
+  const response = await fetch(URL); // 첫 fetch만 진짜 api를 가져오고 나머지는 JS가 기억하고 있다가 캐싱 데이터를 가져오는거
   const json = await response.json();
   return json;
   /* 위와 동일
